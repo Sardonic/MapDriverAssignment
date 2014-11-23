0076116feef924f672c5ee72b0266c0af1068ca0
David Johnston <nuts4nuts4nuts@gmail.com>
2014-11-22 19:29:07 -0500

    added genmap changes from class

--- genmap.sh
+++ genmap.sh
@@ -3,12 +3,6 @@
 MAP_WIDTH=50
 MAP_HEIGHT=50
 
-function genPseudoRandomASCII {
-    # get chars from urandom, suppress error output and pipe to base64 to get rid of escape chars
-    local RAND_CHAR=$(base64 /dev/urandom | head -c 1)
-    echo $RAND_CHAR
-}
-
 if [[ ! -z $1 ]]; then #arg1 exists
     if [[ $1 =~ ^-?[0-9]+$ ]]; then #arg is an integer
 	MAP_WIDTH=$1
@@ -21,39 +15,58 @@ if [[ ! -z $2 ]]; then #arg2 exists
     fi
 fi
 
+function genPseudoRandomASCII {
+    # get chars from urandom, suppress error output and pipe to base64 to get rid of escape chars
+    local RAND_CHAR=$(base64 /dev/urandom | head -c 1)
+    echo -n $RAND_CHAR
+}
+
+function getValOrSpace {
+	local CHAR=$(genPseudoRandomASCII)
+	local SPACE=' '
+
+	local COIN=$RANDOM
+	local PUT_SPACE=$(( $COIN % 2))
+
+	if [[ $PUT_SPACE -eq 0 ]]; then
+		CHAR=$SPACE
+	fi
+
+	echo -n "$CHAR"
+}
+
+function genMap {
+	MAP_STR=""
+
+	for((i=0; i<$MAP_HEIGHT; i++)) do
+		for((j=0; j<$MAP_WIDTH; j++)) do
+			VAR=$(getValOrSpace)
+			printf -v line "%c" $VAR
+			MAP_STR="$MAP_STR$line"
+		done
+
+		printf -v line "\n"
+		MAP_STR="$MAP_STR$line"
+	done
+
+	echo -n "$MAP_STR"
+}
+
 echo "One moment, generating map..."
-MAP_STR=""
-
-for((i=0; i<$MAP_HEIGHT; i++)) do
-    for((j=0; j<$MAP_WIDTH; j++)) do
-	VAR=$(genPseudoRandomASCII)
-	printf -v line "$VAR" $VAR
-	MAP_STR="$MAP_STR$line"
-    done
-    printf -v line '\n'
-    MAP_STR="$MAP_STR$line"
-done
-
-NUM_SPACES=$(echo $MAP_STR | tr -cd ' ' | wc -c) # for some reason tr is counting the newlines
+MAP_STR=$(genMap)
+NUM_SPACES=$(echo -n $MAP_STR | tr -cd ' ' | wc -c) # for some reason tr is counting the newlines
 MAX_SPACES=$(( $MAP_HEIGHT + $MAP_HEIGHT * $MAP_WIDTH / 2)) # compensates for the tr thing, check for more spaces
 ITR=1
 
 while [ $NUM_SPACES -gt $MAX_SPACES ]
 do
     if [[ $ITR == 10 ]]; then
-	>&2 echo "Couldn't Generate a file!"
-	exit -1
+		>&2 echo "Couldn't Generate a file!"
+		exit -1
     fi
 
-    for((i=0; i<$MAP_HEIGHT; i++)) do
-	for((j=0; j<$MAP_WIDTH; j++)) do
-	    VAR=$(genPseudoRandomASCII)
-	    printf -v line "%c" $VAR
-	    MAP_STR="$MAP_STR$line"
-	done
-	printf -v line '\n'
-	MAP_STR="$MAP_STR$line"
-    done
+	echo "One moment, generating map..."
+	MAP_STR=$(genMap)
 
     NUM_SPACES=$(echo $MAP_STR | tr -cd ' ' | wc -c) # for some reason tr is counting the newlines
     ITR=$(( $ITR + 1))
